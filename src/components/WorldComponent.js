44a1c5b1bceb86a557106b8f12cdc54b07ea0ad4
Vishal <poison.iitr@gmail.com>
2019-08-28 00:53:40 +0530

    Add World Component

--- src/components/WorldComponent.js
+++ src/components/WorldComponent.js
@@ -4,21 +4,22 @@ class World extends Component {
     
     constructor(props) {
         super(props);
-        this.X = 1000;
-        this.Y = 500;
+
+        this.width = 1000;
+        this.height = 500;
         this.BGColor = "#228B22";
     }
 
     componentDidMount() {
         const ctx = this.canvas.getContext("2d");
         ctx.fillStyle = this.BGColor;
-        ctx.fillRect(0,0,this.X,this.Y);
+        ctx.fillRect(0,0,this.width,this.height);
     }
 
     render() {
         return(
             <div id="World">
-                <canvas ref={(canvas) => this.canvas = canvas} width={this.X} height={this.Y}/>
+                <canvas ref={(canvas) => this.canvas = canvas} width={this.width} height={this.height}/>
             </div>
         );
     }
