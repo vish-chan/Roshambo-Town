520cfbf12eb0858734437ccabd10b616f936447b
Vishal <poison.iitr@gmail.com>
2019-10-14 00:45:10 +0530

    Change loading component

--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Loading = (props) => {
     return(
-    <div style={{width: '100%', height:'100%', backgroundColor:'white'}}>
-        <div id="loading" style={{position: 'absolute', right: 10, bottom: 10, display:'flex', padding: '5px'}}>
-            <i className="fa fa-spinner fa-pulse fa-5x fa-fw"/>
-            <p style={{fontSize: '20px', fontFamily:'gameboy'}}> {props.msg}...</p>
+    <div style={{width: '100%', height:'100%',  backgroundImage: 'linear-gradient(#991b1e, #b61d20, #991b1e)'}}>
+        <div id="loading" style={{position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', display:'flex', flexDirection:'column', padding: '5px', alignItems:'center'}}>
+            <div  style={{}}><img className="rotate" style={{width:250, height:250}} src="/assets/images/80/objectsAndProps/loading.png"></img></div>
+            <p style={{fontSize: '25px', fontFamily:'gameboy', color:'white'}}> {props.msg}...</p>
         </div>
     </div>);
 }
