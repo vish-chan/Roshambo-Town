e14fb429fd51f6c8e99add6c1de2b6daf08593ff
Vishal <poison.iitr@gmail.com>
2019-10-21 18:06:08 +0530

    Updates

---
+++ precache-manifest.00d35cb431769049668398256a66ca92.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b486ca211058f9e3e34443279cbb3e60",
+    "url": "/Roshambo-Town/index.html"
+  },
+  {
+    "revision": "c83c3215d7f0a0095aa5",
+    "url": "/Roshambo-Town/static/css/2.204c08ce.chunk.css"
+  },
+  {
+    "revision": "7d67c1897419889096d0",
+    "url": "/Roshambo-Town/static/css/main.8dee37cc.chunk.css"
+  },
+  {
+    "revision": "c83c3215d7f0a0095aa5",
+    "url": "/Roshambo-Town/static/js/2.f3a30932.chunk.js"
+  },
+  {
+    "revision": "7d67c1897419889096d0",
+    "url": "/Roshambo-Town/static/js/main.9f739e0f.chunk.js"
+  },
+  {
+    "revision": "ee56de6dd20fef45a297",
+    "url": "/Roshambo-Town/static/js/runtime~main.eefc1796.js"
+  },
+  {
+    "revision": "aa1519137a01780c9b51e7a90d41f29d",
+    "url": "/Roshambo-Town/static/media/BigSpace.aa151913.otf"
+  },
+  {
+    "revision": "d228fa37180443f2fc19671530972763",
+    "url": "/Roshambo-Town/static/media/GamegirlClassic-9MVj.d228fa37.ttf"
+  },
+  {
+    "revision": "ac0c25b469bc056e08618b15c538872d",
+    "url": "/Roshambo-Town/static/media/PixelEmulator.ac0c25b4.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Roshambo-Town/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Roshambo-Town/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Roshambo-Town/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Roshambo-Town/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Roshambo-Town/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
