782d0c94a9c9db4fdd1a36b0099cd00262913fb7
Vishal <poison.iitr@gmail.com>
2019-10-24 02:14:14 +0530

    Preload audio

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
 import Main from './components/MainComponent';
+import AudioEffects from './components/AudioEffects';
 import { ConfigureStore } from './redux/ConfigureStore';
 
 function App() {
   return (
     <Provider store={ConfigureStore()}>
         <Main />
+        <AudioEffects />
     </Provider>
   );
 }
---
+++ src/components/AudioEffects.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { DRIP_SOUND, BEEP_SOUND, BEEP_2_SOUND, BEEP_3_SOUND, BEEP_LONG_SOUND, PICK_SOUND, LASER_SOUND } from '../helpers/constants';
+
+const AudioEffects = () => {
+    return(
+        <React.Fragment>
+            <audio src={DRIP_SOUND} preload="auto"/>
+            <audio src={BEEP_SOUND} preload="auto"/>
+            <audio src={BEEP_2_SOUND} preload="auto"/>
+            <audio src={BEEP_3_SOUND} preload="auto"/>
+            <audio src={BEEP_LONG_SOUND} preload="auto"/>
+            <audio src={PICK_SOUND} preload="auto"/>
+            <audio src={LASER_SOUND} preload="auto"/>
+        </React.Fragment>
+    );
+}
+
+export default AudioEffects;
