0a4a4ffa3c9b0ce4a89e2fb38209116482bc7c06
Vishal <poison.iitr@gmail.com>
2019-10-26 16:49:38 +0530

    Add touch support for modal

--- src/components/WorldComponent.js
+++ src/components/WorldComponent.js
@@ -29,12 +29,14 @@ class World extends Component {
         this.touchstartY = null;
         this.touchendX = null;
         this.touchendY = null;
+        this.timeout = null;
 
         this.handleKeyDown = this.handleKeyDown.bind(this);
         this.handleTouchStart = this.handleTouchStart.bind(this);
         this.handleTouchMove = this.handleTouchMove.bind(this);
         this.handleTouchEnd = this.handleTouchEnd.bind(this);
-        this.toggleExitModalState = this.toggleExitModalState.bind(this);
+        this.setModalState = this.setModalState.bind(this);
+        this.resetModalState = this.resetModalState.bind(this);
         this.state = {
             showExitModal: false,
         }
@@ -56,46 +58,62 @@ class World extends Component {
 
     }
 
-    toggleExitModalState() {
+    resetModalState() {
+        this.setState({
+            showExitModal: false,
+        })
+    }
+
+    setModalState() {
+        this.timeout = null;
         playSoundEffect(BEEP_3_SOUND);
         this.setState({
-            showExitModal: !this.state.showExitModal,
+            showExitModal: true,
         })
     }
 
 
     handleTouchStart(event) {
-        event.preventDefault();
-        this.touchstartX = this.touchendX = event.touches[0].clientX;
-        this.touchstartY = this.touchendY = event.touches[0].clientY;
+        if(!this.state.showExitModal) {
+            event.preventDefault();
+            this.touchstartX = this.touchendX = event.touches[0].clientX;
+            this.touchstartY = this.touchendY = event.touches[0].clientY;
+            this.timeout = setTimeout(this.setModalState, 500);
+        }
     }
 
     handleTouchMove(event) {
-        event.preventDefault();
-        this.touchendX = event.touches[0].clientX;
-        this.touchendY = event.touches[0].clientY;
-        var xDiff = this.touchendX - this.touchstartX, 
-            yDiff = this.touchendY - this.touchstartY;
-        if(Math.abs(xDiff) > Math.abs(yDiff)) {
-            if(xDiff > 0) {
-                this.props.updatePlayerPosition(KEYCODES.right);
-            } else if(xDiff < 80) {
-                this.props.updatePlayerPosition(KEYCODES.left);
-            }
-        } else {
-            if(yDiff > 0) {
-                this.props.updatePlayerPosition(KEYCODES.down);
-            } else if(yDiff < 0) {
-                this.props.updatePlayerPosition(KEYCODES.up);
+        if(this.timeout!==null && !this.state.showExitModal) {
+            event.preventDefault();
+            clearTimeout(this.timeout);
+            this.touchendX = event.touches[0].clientX;
+            this.touchendY = event.touches[0].clientY;
+            var xDiff = this.touchendX - this.touchstartX, 
+                yDiff = this.touchendY - this.touchstartY;
+            if(Math.abs(xDiff) > Math.abs(yDiff)) {
+                if(xDiff > 0) {
+                    this.props.updatePlayerPosition(KEYCODES.right);
+                } else if(xDiff < 80) {
+                    this.props.updatePlayerPosition(KEYCODES.left);
+                }
+            } else {
+                if(yDiff > 0) {
+                    this.props.updatePlayerPosition(KEYCODES.down);
+                } else if(yDiff < 0) {
+                    this.props.updatePlayerPosition(KEYCODES.up);
+                }
             }
         }
     }
 
-    handleTouchEnd() {
+    handleTouchEnd(event) {
         var xDiff = Math.abs(this.touchendX - this.touchstartX), 
             yDiff = Math.abs(this.touchendY - this.touchstartY);
-        if(xDiff===0 && yDiff===0)
+        if(this.timeout!==null && !this.state.showExitModal) {
+            clearTimeout(this.timeout);
+            if(xDiff===0 && yDiff===0)
             this.props.handleConversation();
+        }
     }
 
     handleKeyDown(event) {
@@ -114,7 +132,7 @@ class World extends Component {
             } else if(INVENTORY_KEY.includes(keyCode)) {
                 this.props.toggleInventory();
             } else if(ESC_KEY.includes(keyCode)) {
-                this.toggleExitModalState();
+                this.setModalState();
             }
         } 
     }
@@ -138,7 +156,7 @@ class World extends Component {
                     <Player viewport={this.props.viewport} />
                     <ConversationDisplay />
                     <Inventory />
-                    <CustomModal show={this.state.showExitModal} speedChange={this.props.handlePlayerSpeedChange} confirmLink={this.props.handleBack} cancelLink={this.toggleExitModalState} />
+                    <CustomModal show={this.state.showExitModal} speedChange={this.props.handlePlayerSpeedChange} confirmLink={this.props.handleBack} cancelLink={this.resetModalState} />
             </div>
         );
         }
